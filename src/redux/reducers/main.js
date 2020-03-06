ed197c24ce57031f46382336201aa910620432e3
Timοσ Lanitis <timoscon@gmail.com>
2020-03-06 17:29:09 +0200

    comments and smaller functions

--- src/redux/reducers/main.js
+++ src/redux/reducers/main.js
@@ -35,6 +35,92 @@ const initialState = {
     countriesTaxonomy: []
 };
 
+/* --------- UTILITY FUNCTIONS ---------- */
+const checkProductAgainstTaxonomyAndAutofill = (product, productsTaxonomy) => {
+    if (!product.foodakai) {
+        const original = product.original.toLowerCase();
+        let { bestMatch } = stringSimilarity.findBestMatch(original, productsTaxonomy);
+
+        // transform , seperated ex. Beans, Dried => Dried Beans
+        if (original.indexOf(', ') !== -1) {
+            const array = original.split(', ').reverse();
+            const comma_bestMatch = stringSimilarity.findBestMatch(array.join(' '), productsTaxonomy).bestMatch;
+            if (comma_bestMatch.rating > bestMatch.rating) bestMatch = comma_bestMatch;
+        }
+
+        if (bestMatch.rating > 0.78) product.foodakai = bestMatch.target;
+        console.log(bestMatch);
+    }
+};
+const checkHazardAgainstTaxonomyAndAutofill = (hazard, hazardsTaxonomy) => {
+    if (!hazard.foodakai) {
+        const original = hazard.original.toLowerCase();
+        // specific hack for specific hazard
+        if ((original.indexOf('produced without') !== -1) && (original.indexOf('benefit')  !== -1) && (original.indexOf('inspection')  !== -1)) {
+            hazard.foodakai = 'unauthorised use of federal inspection mark';
+        } else { // check for matching
+            let {bestMatch} = stringSimilarity.findBestMatch(original, hazardsTaxonomy);
+
+            // transform f to ph and check
+            if (original.indexOf('f') !== -1) {
+                const ph_original = original.replace('f', 'ph');
+                const ph_bestMatch = stringSimilarity.findBestMatch(ph_original, hazardsTaxonomy).bestMatch;
+                if (ph_bestMatch.rating > bestMatch.rating) bestMatch = ph_bestMatch;
+            }
+
+            // transform f to ph and check
+            if (original.indexOf('ph') !== -1) {
+                const f_original = original.replace('ph', 'f');
+                const f_bestMatch = stringSimilarity.findBestMatch(f_original, hazardsTaxonomy).bestMatch;
+                if (f_bestMatch.rating > bestMatch.rating) bestMatch = f_bestMatch;
+            }
+
+            // check for string + 'products thereof'
+            if (bestMatch.rating < 0.8) {
+                const products_thereof_bestMatch = stringSimilarity.findBestMatch(original + ' and products thereof', hazardsTaxonomy).bestMatch;
+                if ((products_thereof_bestMatch.rating > 0.85) && (products_thereof_bestMatch.rating > bestMatch.rating)) bestMatch = products_thereof_bestMatch;
+            }
+
+            if (bestMatch.rating > 0.8) hazard.foodakai = bestMatch.target;
+            console.log(bestMatch);
+        }
+    }
+};
+const removeTags = (tag, incident_id, array, titles, descriptions) => {
+    // remove tags
+    const regex = `(<${tag}>(?!${array[0].original})|(?<!${array[0].original})<\\/${tag}>)`;
+    const title = titles[incident_id] ? titles[incident_id].replace(new RegExp(regex, 'g'), '') : null;
+    const description = descriptions[incident_id] ? descriptions[incident_id].replace(new RegExp(regex, 'g'), '') : null;
+
+    return {
+        titles: { ...titles, [incident_id]: title },
+        descriptions: { ...descriptions, [incident_id]: description },
+    }
+};
+const removeTag = (tag, incident_id, product, titles, descriptions) => {
+    const result = {};
+    const tagged = `<${tag}>${product.original}</${tag}>`;
+
+    // check description for tagged text
+    if (descriptions[incident_id] && descriptions[incident_id].indexOf(tagged) !== -1) {
+        // find the description of the incident and replace the tags with the untagged text
+        const description = descriptions[incident_id].replace(tagged, product.original);
+        // create new array of descriptions to replace old one
+        result.descriptions = { ...descriptions, [incident_id]: description };
+    }
+
+    //check title for tagged text
+    if (titles[incident_id] && titles[incident_id].indexOf(tagged) !== -1) {
+        // find the title of the incident and replace the tags with the untagged text
+        const title = titles[incident_id].replace(tagged, product.original);
+        // create new array of titles to replace old one
+        result.titles = { ...titles, [incident_id]: title };
+    }
+
+    return result;
+};
+
+/* ---------- MAIN REDUCER ---------- */
 const main = (state = initialState, action) => {
     switch (action.type) {
         case SET_COMMUNITY: {
@@ -45,6 +131,7 @@ const main = (state = initialState, action) => {
             };
         }
         case SET_INCIDENTS: {
+            /* SET INCIDENTS IS THE MAIN IMPORTING FUNCTION OF THE TOOL. IT FILLS ALL THE OTHER ARRAYS */
             const incidentsToSet = action.payload.incidents;
             const previouslySavedIncidents = action.payload.savedIncidents;
             const incidents = [];
@@ -64,7 +151,7 @@ const main = (state = initialState, action) => {
                 titles[incident.dataId] = incident.title;
                 descriptions[incident.dataId] = incident.description;
             });
-
+            /* LOAD PREVIOUSLY SAVED INCIDENTS FROM MONGO ACCORDING TO INCIDENT IDS */
             if (previouslySavedIncidents) {
                 previouslySavedIncidents.forEach(incident => {
                     const stateIncident = incidents.find(sti => sti.id === incident.id);
@@ -90,6 +177,7 @@ const main = (state = initialState, action) => {
             };
         }
         case ADD_INCIDENTS: {
+            /* MOSTLY USED ON LOAD MORE INCIDENTS WHEN PAGE NUMBER IS BIG ENOUGH TO LOAD MORE INCIDENTS FROM THE DATA PLATFORM */
             const incidentsToAdd = action.payload.incidents;
             const incidents = [...state.incidents];
             const products = { ...state.products };
@@ -145,28 +233,12 @@ const main = (state = initialState, action) => {
 
             // check incident's products array against Taxonomy and autofill
             productsArray.forEach(product => {
-                if (!product.foodakai) {
-                    const original = product.original.toLowerCase();
-                    let { bestMatch } = stringSimilarity.findBestMatch(original, state.productsTaxonomy);
-
-                    // transform , seperated ex. Beans, Dried => Dried Beans
-                    if (original.indexOf(', ') !== -1) {
-                        const array = original.split(', ').reverse();
-                        const comma_bestMatch = stringSimilarity.findBestMatch(array.join(' '), state.productsTaxonomy).bestMatch;
-                        if (comma_bestMatch.rating > bestMatch.rating) bestMatch = comma_bestMatch;
-                    }
-
-                    if (bestMatch.rating > 0.78) product.foodakai = bestMatch.target;
-                    console.log(bestMatch);
-                }
+                checkProductAgainstTaxonomyAndAutofill(product, state.productsTaxonomy);
             });
 
-            // remove tags
-            const regex = `(<product>(?!${productsArray[0].original})|(?<!${productsArray[0].original})<\\/product>)`;
-            const title = state.titles[incident_id] ? state.titles[incident_id].replace(new RegExp(regex, 'g'), '') : null;
-            const titles = { ...state.titles, [incident_id]: title };
-            const description = state.descriptions[incident_id] ? state.descriptions[incident_id].replace(new RegExp(regex, 'g'), '') : null;
-            const descriptions = { ...state.descriptions, [incident_id]: description };
+            // remove all product tags
+            const tag = 'product';
+            const { titles, descriptions } = removeTags(tag, incident_id, productsArray, state.titles, state.descriptions);
 
             // new products
             const products = { ...state.products, [incident_id]: productsArray };
@@ -183,20 +255,7 @@ const main = (state = initialState, action) => {
             // already exists, don't add more
             if (!state.products[incident_id].find(pr => pr.original === product.original)) {
                 // check incident's product against Taxonomy and autofill
-                if (!product.foodakai) {
-                    const original = product.original.toLowerCase();
-                    let { bestMatch } = stringSimilarity.findBestMatch(original, state.productsTaxonomy);
-
-                    // transform , seperated ex. Beans, Dried => Dried Beans
-                    if (original.indexOf(', ') !== -1) {
-                        const array = original.split(', ').reverse();
-                        const comma_bestMatch = stringSimilarity.findBestMatch(array.join(' '), state.productsTaxonomy).bestMatch;
-                        if (comma_bestMatch.rating > bestMatch.rating) bestMatch = comma_bestMatch;
-                    }
-
-                    if (bestMatch.rating > 0.78) product.foodakai = bestMatch.target;
-                    console.log(bestMatch);
-                }
+                checkProductAgainstTaxonomyAndAutofill(product, state.productsTaxonomy);
 
                 const products = { ...state.products, [incident_id]: [...state.products[incident_id], product] };
                 return {
@@ -222,46 +281,12 @@ const main = (state = initialState, action) => {
 
             // check incident's hazards array against Taxonomy and autofill
             hazardsArray.forEach(hazard => {
-                if (!hazard.foodakai) {
-                    const original = hazard.original.toLowerCase();
-                    // specific hack for specific hazard
-                    if ((original.indexOf('produced without') !== -1) && (original.indexOf('benefit')  !== -1) && (original.indexOf('inspection')  !== -1)) {
-                        hazard.foodakai = 'unauthorised use of federal inspection mark';
-                    } else { // check for matching
-                        let {bestMatch} = stringSimilarity.findBestMatch(original, state.hazardsTaxonomy);
-
-                        // transform f to ph and check
-                        if (original.indexOf('f') !== -1) {
-                            const ph_original = original.replace('f', 'ph');
-                            const ph_bestMatch = stringSimilarity.findBestMatch(ph_original, state.hazardsTaxonomy).bestMatch;
-                            if (ph_bestMatch.rating > bestMatch.rating) bestMatch = ph_bestMatch;
-                        }
-
-                        // transform f to ph and check
-                        if (original.indexOf('ph') !== -1) {
-                            const f_original = original.replace('ph', 'f');
-                            const f_bestMatch = stringSimilarity.findBestMatch(f_original, state.hazardsTaxonomy).bestMatch;
-                            if (f_bestMatch.rating > bestMatch.rating) bestMatch = f_bestMatch;
-                        }
-
-                        // check for string + 'products thereof'
-                        if (bestMatch.rating < 0.8) {
-                            const products_thereof_bestMatch = stringSimilarity.findBestMatch(original + ' and products thereof', state.hazardsTaxonomy).bestMatch;
-                            if ((products_thereof_bestMatch.rating > 0.85) && (products_thereof_bestMatch.rating > bestMatch.rating)) bestMatch = products_thereof_bestMatch;
-                        }
-
-                        if (bestMatch.rating > 0.8) hazard.foodakai = bestMatch.target;
-                        console.log(bestMatch);
-                    }
-                }
+                checkHazardAgainstTaxonomyAndAutofill(hazard, state.hazardsTaxonomy);
             });
 
-            //remove tags
-            const regex = `(<hazard>(?!${hazardsArray[0].original})|(?<!${hazardsArray[0].original})<\\/hazard>)`;
-            const title = state.titles[incident_id] ? state.titles[incident_id].replace(new RegExp(regex, 'g'), '') : null;
-            const titles = { ...state.titles, [incident_id]: title };
-            const description = state.descriptions[incident_id] ? state.descriptions[incident_id].replace(new RegExp(regex, 'g'), '') : null;
-            const descriptions = { ...state.descriptions, [incident_id]: description };
+            // remove all hazard tags
+            const tag = 'hazard';
+            const { titles, descriptions } = removeTags(tag, incident_id, hazardsArray, state.titles, state.descriptions);
 
             // new hazards
             const hazards = { ...state.hazards, [incident_id]: hazardsArray };
@@ -278,39 +303,7 @@ const main = (state = initialState, action) => {
             // already exists, don't add more
             if (!state.hazards[incident_id].find(hz => hz.original === hazard.original)) {
                 // check incident's hazard against Taxonomy and autofill
-                if (!hazard.foodakai) {
-                    const original = hazard.original.toLowerCase();
-                    // specific hack for specific hazard
-                    if ((original.indexOf('produced without') !== -1) && (original.indexOf('benefit')  !== -1) && (original.indexOf('inspection')  !== -1)) {
-                        hazard.foodakai = 'unauthorised use of federal inspection mark';
-                    } else { // check for matching
-                        let {bestMatch} = stringSimilarity.findBestMatch(original, state.hazardsTaxonomy);
-
-                        // transform f to ph and check
-                        if (original.indexOf('f') !== -1) {
-                            const ph_original = original.replace('f', 'ph');
-                            const ph_bestMatch = stringSimilarity.findBestMatch(ph_original, state.hazardsTaxonomy).bestMatch;
-                            if (ph_bestMatch.rating > bestMatch.rating) bestMatch = ph_bestMatch;
-                        }
-
-                        // transform f to ph and check
-                        if (original.indexOf('ph') !== -1) {
-                            const f_original = original.replace('ph', 'f');
-                            const f_bestMatch = stringSimilarity.findBestMatch(f_original, state.hazardsTaxonomy).bestMatch;
-                            if (f_bestMatch.rating > bestMatch.rating) bestMatch = f_bestMatch;
-                        }
-
-                        // check for string + 'products thereof'
-                        if (bestMatch.rating < 0.8) {
-                            const products_thereof_bestMatch = stringSimilarity.findBestMatch(original + ' and products thereof', state.hazardsTaxonomy).bestMatch;
-                            if ((products_thereof_bestMatch.rating > 0.85) && (products_thereof_bestMatch.rating > bestMatch.rating)) bestMatch = products_thereof_bestMatch;
-                        }
-
-                        if (bestMatch.rating > 0.8) hazard.foodakai = bestMatch.target;
-
-                        console.log(bestMatch);
-                    }
-                }
+                checkHazardAgainstTaxonomyAndAutofill(hazard, state.hazardsTaxonomy);
 
                 const hazards = { ...state.hazards, [incident_id]: [...state.hazards[incident_id], hazard] };
                 return {
@@ -379,25 +372,9 @@ const main = (state = initialState, action) => {
             // create new array of products to replace the old one
             const products = { ...state.products, [incident_id]: listOfSpecficIncidentProducts };
 
-            const tagged = '<product>' + product.original + '</product>';
-
-            // check description for tagged text
-            let descriptions = null;
-            if (state.descriptions[incident_id] && state.descriptions[incident_id].indexOf(tagged) !== -1) {
-                // find the description of the incident and replace the tags with the untagged text
-                const description = state.descriptions[incident_id].replace(tagged, product.original);
-                // create new array of descriptions to replace old one
-                descriptions = { ...state.descriptions, [incident_id]: description };
-            }
-
-            //check title for tagged text
-            let titles = null;
-            if (state.titles[incident_id] && state.titles[incident_id].indexOf(tagged) !== -1) {
-                // find the title of the incident and replace the tags with the untagged text
-                const title = state.titles[incident_id].replace(tagged, product.original);
-                // create new array of titles to replace old one
-                titles = { ...state.titles, [incident_id]: title };
-            }
+            // remove specific tag
+            const tag = 'product';
+            const { descriptions, titles } = removeTag(tag, incident_id, product, state.titles, state.descriptions);
 
             // return new object with the regarded changes
             const res = { ...state, products };
@@ -413,25 +390,9 @@ const main = (state = initialState, action) => {
             // create new array of hazards to replace the old one
             const hazards = { ...state.hazards, [incident_id]: listOfSpecficIncidentHazards };
 
-            const tagged = '<hazard>' + hazard.original + '</hazard>';
-
-            // check description for tagged text
-            let descriptions = null;
-            if (state.descriptions[incident_id] && state.descriptions[incident_id].indexOf(tagged) !== -1) {
-                // find the description of the incident and replace the tags with the untagged text
-                const description = state.descriptions[incident_id].replace(tagged, hazard.original);
-                // create new array of descriptions to replace old one
-                descriptions = { ...state.descriptions, [incident_id]: description };
-            }
-
-            //check title for tagged text
-            let titles = null;
-            if (state.titles[incident_id] && state.titles[incident_id].indexOf(tagged) !== -1) {
-                // find the title of the incident and replace the tags with the untagged text
-                const title = state.titles[incident_id].replace(tagged, hazard.original);
-                // create new array of titles to replace old one
-                titles = { ...state.titles, [incident_id]: title };
-            }
+            // remove specific tag
+            const tag = 'hazard';
+            const { descriptions, titles } = removeTag(tag, incident_id, hazard, state.titles, state.descriptions);
 
             // return new object with the regarded changes
             const res = { ...state, hazards };
