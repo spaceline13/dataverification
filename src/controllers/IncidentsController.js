dded7e393e959addf37ddae55a624d0895e5315d
Timοσ Lanitis <timoscon@gmail.com>
2020-03-11 16:03:01 +0200

    remoteHazards and oneHazard

--- src/controllers/IncidentsController.js
+++ src/controllers/IncidentsController.js
@@ -10,7 +10,6 @@ export const fetchIncidentsIncludingUnpublished = ({ freetext = '', product, haz
     const productString = ifArrayCreateStringForQuery(product);
     const hazardString = ifArrayCreateStringForQuery(hazard);
     const sourceString = ifArrayCreateStringForQuery(source);
-    console.log(product, hazard);
 
     const options = {
         smart: false,
